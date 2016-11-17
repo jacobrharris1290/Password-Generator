1ddf8e1fbdd2ab81e0cd1dd33c3dc952b4401d71
Jake Harris <jacobharris@Jacobs-MacBook-Pro.local>
2016-11-17 15:26:01 -0800

    Added changes

--- Product Name Generator (Hack Day)/ViewController.swift
+++ Product Name Generator (Hack Day)/ViewController.swift
@@ -8,29 +8,45 @@
 
 import UIKit
 
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = self.index(startIndex, offsetBy: r.lowerBound)
+        let end = self.index(startIndex, offsetBy: (r.upperBound-r.lowerBound))
+        return self[Range(start ..< end)]
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var passwordLength: UITextField!
     @IBOutlet weak var charactersToUse: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
     
-    func generatePassword (nums: Array<Character>, length: Int) -> (String){
+    
+    
+    func generatePassword (nums: String, length: Int) -> (String){
         var pswd = ""
         for _ in 1...length{
-            pswd = pswd + String(arc4random_uniform(UInt32(nums.count)))
+            var n = Int(arc4random_uniform(UInt32(length)))
+            pswd += nums[n]
+            //pswd = pswd + String(arc4random_uniform(UInt32(nums.count)))
         }
         return pswd
     }
     
     @IBAction func generatePassword(_ sender: Any) {
         let pswdLength = Int(passwordLength.text!)
-        var digits = String(describing: charactersToUse.text)
-        let chars = Array(digits.characters)
-        print(chars)
-        
-        
-        
-        self.passwordLabel.text = String(generatePassword(nums: chars, length: pswdLength!));
+        let digits = String(describing: charactersToUse.text!)
+        self.passwordLabel.text = String(generatePassword(nums: digits , length: pswdLength!));
         
     }
     
